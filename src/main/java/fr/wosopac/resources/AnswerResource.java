7655956df5eaaa59ead15aaa9cdd357468f086e2
Killian LEGENDRE <klegendre@joys.fr>
2024-01-09 14:36:34 +0100

    :sparkles: continued routes

--- src/main/java/fr/wosopac/resources/AnswerResource.java
+++ src/main/java/fr/wosopac/resources/AnswerResource.java
@@ -1,7 +1,11 @@
 package fr.wosopac.resources;
 
 import fr.wosopac.entities.Answer;
+import fr.wosopac.entities.AppUser;
+import fr.wosopac.entities.Question;
+import fr.wosopac.entities.Quizz;
 import fr.wosopac.services.AnswerService;
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
@@ -21,19 +25,34 @@ public class AnswerResource {
         this.answerService = answerService;
     }
 
-    @GET
-    public RestResponse<List<Answer>> getAllAnswer() {
-        List<Answer> answers = answerService.findAllAnswers();
-        return RestResponse.ok(answers);
-    }
-
     @POST
-    public RestResponse<Void> createIngredient(@Valid List<Answer> answers, @Context UriInfo uriInfo) {
-        Answer lastAddedAnswer = new Answer();
+    @Transactional
+    public RestResponse<Void> createAnswer(@Valid List<Answer> answers, @Context UriInfo uriInfo) {
         for (Answer answer : answers) {
-            lastAddedAnswer = answerService.persistAnswer(answer);
+            Quizz quizz = answerService.findQuizzById(answer.quizz.id);
+            AppUser appUser = answerService.findAppUserById(answer.appUser.id);
+            Question question = answerService.findQuestionById(answer.question.id);
+
+            if (quizz == null || question == null) {
+                return RestResponse.noContent();
+            }
+
+            if (appUser == null) {
+                AppUser newAppUser = new AppUser();
+                newAppUser.id = answer.appUser.id;
+                newAppUser.persist();
+                answer.appUser = newAppUser;
+            } else {
+                answer.appUser = appUser;
+            }
+
+            answer.quizz = quizz;
+            answer.question = question;
+
+            answerService.persistAnswer(answer);
         }
-        UriBuilder builder = uriInfo.getAbsolutePathBuilder().path(lastAddedAnswer.id.getClass());
+
+        UriBuilder builder = uriInfo.getAbsolutePathBuilder().path("answers");
         return RestResponse.created(builder.build());
     }
 }
